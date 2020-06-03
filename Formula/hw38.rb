7ff7cfea8522221e222aa60b3ae247709167e469
Darren Look <darrlook@amazon.com>
2020-06-03 13:15:03 -0700

    Add python 3.8 depends_on

--- Formula/hw38.rb
+++ Formula/hw38.rb
@@ -6,8 +6,8 @@ class Hw38 < Formula
     url "https://darrenlook-homebrew-tap.s3-us-west-2.amazonaws.com/hw38.tar.gz"
     sha256 "84009b89abf14c3c5701f44c227c43627a59b300d862bf350f12609ee1738a1f"
   
-    bottle :unneeded
-  
+    depends_on "python@3.8"
+
     def install
       virtualenv_install_with_resources
     end
