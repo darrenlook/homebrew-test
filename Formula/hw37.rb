a02c41db1b388f32b7d30445df64d6fd21d5dd34
Darren Look <darrlook@amazon.com>
2020-06-04 12:54:10 -0700

    Test private repo

--- Formula/hw37.rb
+++ Formula/hw37.rb
@@ -1,7 +1,7 @@
 class Hw37 < Formula
     include Language::Python::Virtualenv
   
-    desc "Hello World with Python 3.7"
+    desc "Hello World with Python 3.7 - now in a private repo!"
     homepage "https://www.google.com"
     url "https://darrenlook-homebrew-tap.s3-us-west-2.amazonaws.com/hw37.tar.gz"
     sha256 "df50b14d17ae14a9e81b57baf67814425df81ad25c308325e7e3171c70fc5b8a"
--- Formula/hw38.rb
+++ Formula/hw38.rb
@@ -1,7 +1,7 @@
 class Hw38 < Formula
     include Language::Python::Virtualenv
   
-    desc "Hello World with Python 3.8"
+    desc "Hello World with Python 3.8 - now in a private repo!"
     homepage "https://www.google.com"
     url "https://darrenlook-homebrew-tap.s3-us-west-2.amazonaws.com/hw38.tar.gz"
     sha256 "84009b89abf14c3c5701f44c227c43627a59b300d862bf350f12609ee1738a1f"
